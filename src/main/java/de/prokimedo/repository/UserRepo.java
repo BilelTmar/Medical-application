a9e3bc5ce7cce9449f807ed90c4fdd8f04b85b68
seifert <seifert@forwiss.uni-passau.de>
2016-12-15 13:43:15 +0100

    User → ProkimedoUser

--- src/main/java/de/prokimedo/repository/UserRepo.java
+++ src/main/java/de/prokimedo/repository/UserRepo.java
@@ -1,10 +1,10 @@
 package de.prokimedo.repository;
-import de.prokimedo.entity.User;
+import de.prokimedo.entity.ProkimedoUser;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepo extends CrudRepository<User, Long> {
+public interface UserRepo extends CrudRepository<ProkimedoUser, Long> {
 
-    List<User> findByFirstNames(String firstNames);
+    List<ProkimedoUser> findByFirstNames(String firstNames);
 
 }
